a6ffe972961b3508a0679fb4b71b67941ca79aed
Thomas J. Galazka <thomas.galazka@galazoft.com>
2018-02-04 17:41:15 -0500

    Finished the reversal of the sentence, breaking on word boundaries.
    Tested even and odd lengthed sentences.

--- src/main/java/gadgets/shrewd/core/util/Words.java
+++ src/main/java/gadgets/shrewd/core/util/Words.java
@@ -8,19 +8,43 @@ import java.util.Objects;
 public class Words {
 
     /**
-     * Reverses the supplied string, assumed to be a standard English sentence.
+     * Reverses the supplied string, assumed to be a standard English sentence,
+     * based on the boundaries of words; punctuation will also be reversed.
      *
-     * Removes all tailing whitespace before reversal.
+     * Removes all leading/tailing whitespace before reversal.
      *
-     * @param sentence Sentence to reverse, e.g. "Hello, World!" becomes "World! Hello,"
+     * @param sentence Sentence to reverse, e.g. "Hello, World!" becomes "!World, Hello"
      * @return Reversed sentence, or empty String.
      */
     public static String reverse(String sentence) {
-        //Exit fast on null or empty.
-        if (Objects.isNull(sentence) || sentence.isEmpty())
+        //Handle absent sentence.
+        sentence = Objects.isNull(sentence) ? "" : sentence.trim();
+
+        //Exit fast if empty.
+        if (sentence.isEmpty())
             return "";
 
-        String trimmed = sentence.trim();
-        return trimmed;
+        //Spilt into an array based on word boundary Regular Expression
+        String[] split = sentence.trim().split("\\b");
+        int len = split.length;
+
+        //Temporary, single word holder during reversal.
+        String word;
+        //Iterate through list, swapping positions of words; doesn't load second array into memory.
+        for (int i = 0; i < (len/2); i++) {
+            //save original word
+            word = split[i];
+
+            //calculate swap position
+            int swap = len - (i + 1);
+
+            //swap word positions
+            split[i] = split[swap];
+            split[swap] = word;
+
+        }
+
+        //Reassemble sentence; internal word boundaries were preserved so don't inject a delimiter.
+        return String.join("", split);
     }
 }
--- src/test/java/gadgets/shrewd/core/util/Words_ReverseTest.java
+++ src/test/java/gadgets/shrewd/core/util/Words_ReverseTest.java
@@ -18,4 +18,32 @@ public class Words_ReverseTest {
         assertEquals("Whitespace only input should return empty string",
                 "", Words.reverse("   "));
     }
+
+    @Test
+    public void return_single_worded_sentence() {
+        assertEquals("Failed to return original string",
+                "sAmplE", Words.reverse("sAmplE  "));
+    }
+
+    @Test
+    public void reverse_a_simple_sentence() {
+        assertEquals("Failed to reverse a three word (odd numbered) sentence",
+                "!gadgets shrewd, Hello",
+                Words.reverse("Hello, shrewd gadgets!"));
+
+        assertEquals("Failed to reverse a four word (even numbered) sentence",
+                "!gadgets shrewd, again Hello",
+                Words.reverse("Hello again, shrewd gadgets!"));
+    }
+
+    @Test
+    public void reverse_a_longer_sentence() {
+        String gba = "Fourscore and seven years ago our fathers brought forth, on this continent, a new nation, " +
+                "conceived in liberty, and dedicated to the proposition that all men are created equal.";
+
+        String rev = ".equal created are men all that proposition the to dedicated and, liberty in conceived, " +
+                "nation new a, continent this on, forth brought fathers our ago years seven and Fourscore";
+        assertEquals("Failed to reverse the Gettysburg Address",
+                rev, Words.reverse(gba));
+    }
 }
