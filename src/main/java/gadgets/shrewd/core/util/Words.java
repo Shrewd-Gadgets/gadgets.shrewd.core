aae384530bb4606fb17abe3aef2ae712250771ef
Thomas J. Galazka <thomas.galazka@galazoft.com>
2018-02-04 20:15:24 -0500

    Added the intersection function to the Words utility class.
    Made the internal conversion of a string to a set of characters package-protected to be tested seperately.

--- src/main/java/gadgets/shrewd/core/util/Words.java
+++ src/main/java/gadgets/shrewd/core/util/Words.java
@@ -1,6 +1,9 @@
 package gadgets.shrewd.core.util;
 
+import java.util.Collections;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Utility class containing static word manipulation functions.
@@ -46,5 +49,58 @@ public class Words {
 
         //Reassemble sentence; internal word boundaries were preserved so don't inject a delimiter.
         return String.join("", split);
+    }
+
+
+    /**
+     * Accepts two strings and returns a string containing all the characters present in
+     * both strings; this includes whitespace characters.
+     *
+     * If the Strings have no characters an empty String is returned.
+     *
+     * @param primary First string used for comparison, e.g. "HELLO, world!"
+     * @param secondary Second string used for comparison, e.g. "hello, WORLD!"
+     * @return String containing common characters, e.g. "LO, ol!"
+     */
+    public static String intersection(String primary, String secondary) {
+        //Exit fast if inputs are absent
+        if (Objects.isNull(primary) ||
+                Objects.isNull(secondary) ||
+                primary.isEmpty() ||
+                secondary.isEmpty()) {
+            return "";
+        }
+
+        //Exit fast if the inputs match exactly
+        if (primary.equals(secondary))
+            return primary;
+
+        //Sets are naturally distinct so isolate the characters from each string.
+        Set<Character> prime = setify(primary);
+        Set<Character> other = setify(secondary);
+
+        //Leverage Set function to remove unique characters from primary Set
+        prime.retainAll(other);
+
+        //If all characters removed, return empty string; otherwise join the common characters.
+        return prime.isEmpty() ?
+                "" :
+                prime.stream()
+                        .map(String::valueOf)
+                        .collect(Collectors.joining(""));
+    }
+
+    /**
+     * Transforms a String into a Set of distinct Characters.
+     *
+     * Package-protected for testability.
+     * @param input String to transform
+     * @return Distinct collection of Characters
+     */
+    static Set<Character> setify(String input) {
+        //Ensures there is always a String to transform, avoiding null exceptions.
+        return Objects.isNull(input) ?
+                Collections.emptySet() :
+                input.chars().mapToObj(c -> (char)c).collect(Collectors.toSet());
     }
 }
--- src/test/java/gadgets/shrewd/core/util/WordsTestSuite.java
+++ src/test/java/gadgets/shrewd/core/util/WordsTestSuite.java
@@ -3,13 +3,13 @@ package gadgets.shrewd.core.util;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-        Words_ReverseTest.class
-})
-
 /**
  * Test suite collective for the individual function tests.
  */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        Words_ReverseTest.class,
+        Words_IntersectionTest.class
+})
 public class WordsTestSuite {
 }
